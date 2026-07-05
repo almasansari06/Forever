7519c9d2a0fe05a99c04f635eed4837aadaa90d5
ALMAS ANSARI <115511546+almasansari06@users.noreply.github.com>
2026-07-06 03:14:33 +0530

    Update contact email in Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
                 <li>+91 9588638898</li>
-                <li>almasansari771@gmail.com</li>
+                <li>ansarialmas0206@gmail.com</li>
             </ul>
         </div>
       </div>
