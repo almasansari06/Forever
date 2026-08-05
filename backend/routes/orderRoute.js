70240c9cb8ee62a04daf4bea45e1e60f2978a98d
ALMAS ANSARI <115511546+almasansari06@users.noreply.github.com>
2026-08-06 04:30:08 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,23 +1,25 @@
 import express from 'express'
-import{placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay} from '../controllers/orderController.js'
+import { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay, cancelOrder } from '../controllers/orderController.js'
 import adminAuth from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
 
 const orderRouter = express.Router()
 
-//Admin Features
-orderRouter.post('/list',adminAuth,allOrders)
-orderRouter.post('/status',adminAuth,updateStatus)
+// Admin Features
+orderRouter.post('/list', adminAuth, allOrders)
+orderRouter.post('/status', adminAuth, updateStatus)
 
 // Payment Features
-orderRouter.post('/place',authUser,placeOrder)
-orderRouter.post('/stripe',authUser,placeOrderStripe)
-orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
+orderRouter.post('/place', authUser, placeOrder)
+orderRouter.post('/stripe', authUser, placeOrderStripe)
+orderRouter.post('/razorpay', authUser, placeOrderRazorpay)
 
 // User Features
-orderRouter.post('/userorders',authUser,userOrders)
+orderRouter.post('/userorders', authUser, userOrders)
+orderRouter.post('/cancel', authUser, cancelOrder) // <--- Added Cancel Order Route
 
 // verifyPayment
-orderRouter.post('/verifyStripe',authUser,verifyStripe)
-orderRouter.post('/verifyRazorpay',authUser,verifyRazorpay)
+orderRouter.post('/verifyStripe', authUser, verifyStripe)
+orderRouter.post('/verifyRazorpay', authUser, verifyRazorpay)
+
 export default orderRouter
