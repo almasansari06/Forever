189844be6158f73e070c4523919f735ac4f0ec02
ALMAS ANSARI <115511546+almasansari06@users.noreply.github.com>
2026-08-06 04:01:46 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,7 +8,9 @@ const authUser = (req, res, next) => {
     }
     try {
         const token_decode = jwt.verify(token, process.env.JWT_SECRET)
-        req.body.userId = token_decode.id
+        req.body = req.body || {};
+        req.body.userId = token_decode.id;
+        req.userId = token_decode.id;
         next()
     } catch (error) {
         console.log(error)
