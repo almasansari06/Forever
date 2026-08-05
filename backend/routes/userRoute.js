0d1ec5acf5d429aaca1a746c279c10b2e0a21985
ALMAS ANSARI <115511546+almasansari06@users.noreply.github.com>
2026-08-06 04:01:11 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { loginUser, registerUser, adminLogin } from '../controllers/userController.js';
+import { loginUser, registerUser, adminLogin, getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import authUser from '../middleware/auth.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/register', registerUser)
-userRouter.post('/login', loginUser)
-userRouter.post('/admin', adminLogin)
+userRouter.post('/register', registerUser);
+userRouter.post('/login', loginUser);
+userRouter.post('/admin', adminLogin);
+
+// Profile page routes (Requires Authentication)
+userRouter.get('/get-profile', authUser, getUserProfile);
+userRouter.post('/update-profile', authUser, updateUserProfile);
 
 export default userRouter;
