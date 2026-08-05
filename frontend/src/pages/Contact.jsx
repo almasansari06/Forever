d3c94f97dae2fb5a7f62fb3e1d5fe9cb44442d4b
ALMAS ANSARI <115511546+almasansari06@users.noreply.github.com>
2026-08-06 03:41:03 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -8,19 +8,34 @@ const Contact = () => {
     <div>
       <div className='text-center text-2xl pt-10 border-t'>
         <Title text1={'CONTACT '} text2={'US'}/>
-
       </div>
+
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
-        <img className='w-ful md:max-w-[480px]' src={assets.contact_img} alt="" />
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="Contact Us" />
+        
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className='text-gray-500'>502/4 Bhumi World <br/>Bhiwandi Bypass Road <br /> Bhiwandi 421302 </p>
-          <p className='text-gray-500'>Tel: (+91)9588638898 <br /> Email: almasansari771@gmail.com</p>
-          <p className='font-semibold text-xl text-gray-600'>Carrers at Forever</p>
+          
+          <p className='text-gray-500 leading-relaxed'>
+            Mindspace 12D, Madhapur, <br /> 
+            Hyderabad, Telangana 500081
+          </p>
+          
+          <div className='text-gray-500 leading-relaxed'>
+            <p>Tel: +91 999915299</p>
+            <p>Tel: +976 50-523-4444</p>
+            <p>Email: forevernew@forever.com</p>
+          </div>
+
+          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
-          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+          
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>
+            Explore Jobs
+          </button>
         </div>
       </div>
+
       <NewsletterBox/>
     </div>
   )
