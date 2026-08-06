6134a8e8272d6a07943a7d38015300a0aec654a8
ALMAS ANSARI <115511546+almasansari06@users.noreply.github.com>
2026-08-07 01:32:37 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,12 +4,13 @@ import adminAuth from '../middleware/adminAuth.js';
 
 const userRouter = express.Router();
 
+// User Auth Routes
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 userRouter.post('/admin', adminLogin);
 
-// Admin User Management Routes
-userRouter.get('/all-users', adminAuth, getAllUsers);
+// Admin User Management Routes (Sabi POST Methods)
+userRouter.post('/all-users', adminAuth, getAllUsers); // <--- GET se POST kar diya hai
 userRouter.post('/toggle-status', adminAuth, toggleUserStatus);
 userRouter.post('/delete-user', adminAuth, deleteUser);
 
